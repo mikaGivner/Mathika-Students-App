1f5adda6328015f91add8308c326d5502d8acdd3
mika <mikagivner123@gmail.com>
2023-07-19 15:30:51 +0300

    style button for use

---
+++ src/Style/Button.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import "../index.css"
+const Button = styled.button`
+width:10rem;
+height:2.5rem;
+transform: translate(50, 0);
+color:var(--logo-cr);
+background: linear-gradient(30deg, var(--dark-bg), var(--light-bg));
+border-radius: 1rem;
+border: none;
+box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+`
+export default Button;
--- src/Style/index.jsx
+++ src/Style/index.jsx
@@ -1,4 +1,5 @@
 import Land from "./LandPageHeadStyle";
 import LogoStyle from "./LandPageLogo";
 import LandPageStyle from "./LandStyle";
-export {Land, LogoStyle, LandPageStyle};
+import Button from "./Button";
+export {Land, LogoStyle, LandPageStyle, Button};
