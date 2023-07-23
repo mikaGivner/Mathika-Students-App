c9e49d1b7ef3a67080c528c8deb663d3bb84010b
mika <mikagivner123@gmail.com>
2023-07-23 20:42:31 +0300

    demo users db

---
+++ src/assets/MockTeacherArr.jsx
@@ -0,0 +1,19 @@
+export default MockDBTeacher={
+ users:{
+    MikaGi:{  
+        isTeacher:true,
+        openLessons:{
+            today:[
+                "2pm-",
+                "3pm-Mika Noy",
+                "4pm-"
+            ],
+            tomorrow:[
+                "12pm-Shaked Levy",
+                "14pm-Tommer Hen",
+                "15pm-Danit Kol"
+            ],
+        }
+    }
+ }
+}
